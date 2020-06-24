a5bda925f27df2219b25d12dc615688d9739a9aa
vikas kumar <viomjeet@gmail.com>
2020-06-24 15:31:05 +0530

    Updates

---
+++ precache-manifest.eb702d5067b72a8af96c595ac35298da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3592e3c480caba6738127fc14bda8500",
+    "url": "/ReactCRUDAPP/index.html"
+  },
+  {
+    "revision": "2607f718cba4e85d76ec",
+    "url": "/ReactCRUDAPP/static/css/main.bc11550b.chunk.css"
+  },
+  {
+    "revision": "6e21ac46dbd4f2dc913a",
+    "url": "/ReactCRUDAPP/static/js/2.cc2e5a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCRUDAPP/static/js/2.cc2e5a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2607f718cba4e85d76ec",
+    "url": "/ReactCRUDAPP/static/js/main.5d08a0a3.chunk.js"
+  },
+  {
+    "revision": "b8ed2d9e82fe8e818183",
+    "url": "/ReactCRUDAPP/static/js/runtime-main.84e7ac13.js"
+  }
+]);
